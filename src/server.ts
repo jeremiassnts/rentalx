6ab65813fd42c4f3445ab92f119c568498eb7720
Jeremias Santos <jeremias.santos@newcheck.com.br>
2022-02-01 09:35:35 -0300

    fix: poll para update de server

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,4 @@ const app = express();
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3333, () => console.log("Server is running!!"));
